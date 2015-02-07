9d4793c788004c64744a9e030906813d0d23ceba
csaldana <jonathan.csaldana@alumno.udg.mx>
2015-02-07 13:19:32 -0600

    Primer cambio

---
+++ main.js
@@ -0,0 +1,14 @@
+var red = [0, 100, 63];
+var orange = [40, 100, 60];
+var green = [75, 100, 40];
+var blue = [196, 77, 55];
+var purple = [280, 50, 60];
+
+var myName = "Jonathan";
+letterColors = [red, orange, green, blue, purple];
+if(8 < 3){
+	bubbleShape = "square";
+}
+
+drawName(myName, letterColors);
+bounceBubbles();
